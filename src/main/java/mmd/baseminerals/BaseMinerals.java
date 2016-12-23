7388398b88bb63bc81729f67d393e80e09e1f9d3
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-12-22 16:43:27 -0800

    Fix proxy

--- src/main/java/mmd/baseminerals/BaseMinerals.java
+++ src/main/java/mmd/baseminerals/BaseMinerals.java
@@ -29,7 +29,7 @@ public class BaseMinerals {
 	 */
 	public static final String VERSION = "0.12.0-beta1";
 
-	@SidedProxy(clientSide = "baseminerals.proxy.ClientProxy", serverSide = "baseminerals.proxy.CommonProxy")
+	@SidedProxy(clientSide = "mmd.baseminerals.proxy.ClientProxy", serverSide = "mmd.baseminerals.proxy.CommonProxy")
 	public static CommonProxy PROXY = null;
 
 	@EventHandler
