0e7bca612a12adcd388dde3e3d66d4070f68d014
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-02-09 21:17:13 -0800

    Update plugin loader

--- src/main/java/com/mcmoddev/baseminerals/integration/IntegrationManager.java
+++ src/main/java/com/mcmoddev/baseminerals/integration/IntegrationManager.java
@@ -1,11 +1,12 @@
 package com.mcmoddev.baseminerals.integration;
 
 import com.google.common.collect.Lists;
+import com.mcmoddev.baseminerals.BaseMinerals;
+import com.mcmoddev.baseminerals.integration.BaseMineralsPlugin;
 import com.mcmoddev.lib.integration.IIntegration;
-import com.mcmoddev.lib.util.AnnotationChecker;
 
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.discovery.ASMDataTable.ASMData;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.List;
@@ -16,15 +17,20 @@ public enum IntegrationManager {
     private List<IIntegration> integrations = Lists.newArrayList();
 
     public void preInit(FMLPreInitializationEvent event) {
-        AnnotationChecker.getInstances(event.getAsmData(), BaseMineralsPlugin.class, IIntegration.class).stream()
-                .forEachOrdered(integration -> {
-                    Class<? extends IIntegration> integrationClass = integration.getClass();
-                    BaseMineralsPlugin plugin = integrationClass.getAnnotation(BaseMineralsPlugin.class);
-                    if (Loader.isModLoaded(plugin.value())) {
-                    	FMLLog.severe("BASEMINERALS: Loaded " + plugin.value());
-                        integrations.add(integration);
-                        integration.init();
-                    }
-                });
+    	for( final ASMData asmDataItem : event.getAsmData().getAll(BaseMineralsPlugin.class.getCanonicalName()) ) {
+    		String modId = asmDataItem.getAnnotationInfo().get("value").toString();
+    		String className = asmDataItem.getClassName();
+    		if (Loader.isModLoaded(modId)) {
+    			IIntegration integration;
+				try {
+					integration = Class.forName(className).asSubclass(IIntegration.class).newInstance();
+					BaseMinerals.logger.info("BASEMINERALS Loaded: " + modId);
+					integrations.add(integration);
+					integration.init();
+				} catch (final Exception e) {
+					// do nothing 
+				}
+    		}
+    	}
     }
 }
