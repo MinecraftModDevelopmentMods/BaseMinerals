3ac0295710c378a1b2c3ce2c0bfcc3f99bcb1f53
Daniel Hazelton <dshadowwolf@gmail.com>
2017-03-11 20:46:45 -0500

    fix the logic fail I've been chasing across all of *metals

--- src/main/java/com/mcmoddev/orespawn/BaseMineralsOreSpawn.java
+++ src/main/java/com/mcmoddev/orespawn/BaseMineralsOreSpawn.java
@@ -4,6 +4,7 @@ import com.google.common.base.Function;
 import com.mcmoddev.baseminerals.init.Materials;
 import mmd.orespawn.api.OreSpawnAPI;
 import mmd.orespawn.api.SpawnLogic;
+import com.mcmoddev.baseminerals.util.Config.Options;
 
 public class BaseMineralsOreSpawn implements Function<OreSpawnAPI, SpawnLogic> {
 
@@ -11,14 +12,34 @@ public class BaseMineralsOreSpawn implements Function<OreSpawnAPI, SpawnLogic> {
 	public SpawnLogic apply(OreSpawnAPI api) {
 		SpawnLogic logic = api.createSpawnLogic();
 
-		logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD)
-				.addOre(Materials.lithium.ore.getDefaultState(), 8, 4, 8, 0,  96)
-				.addOre(Materials.niter.ore.getDefaultState(), 8, 4,  8, 0,  96)
-				.addOre(Materials.salt.ore.getDefaultState(), 8, 4, 8, 0, 96)
-				.addOre(Materials.sulfur.ore.getDefaultState(), 8, 4, 6, 0, 32)
-				.addOre(Materials.phosphorus.ore.getDefaultState(), 8, 4, 6, 0, 96)
-				.addOre(Materials.potash.ore.getDefaultState(), 8, 4, 6, 0, 96)
-				.addOre(Materials.saltpeter.ore.getDefaultState(), 8, 4, 6, 0, 96);
+		if( Options.enableLithium ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.lithium.ore.getDefaultState(), 8, 4, 8, 0,  96);
+		}
+
+		if( Options.enableNiter ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.niter.ore.getDefaultState(), 8, 4,  8, 0,  96);
+		}
+
+		if( Options.enableSalt ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.salt.ore.getDefaultState(), 8, 4, 8, 0, 96);
+		}
+
+		if( Options.enableSulfur ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.sulfur.ore.getDefaultState(), 8, 4, 6, 0, 32);
+		}
+
+		if( Options.enablePhosphorus ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.phosphorus.ore.getDefaultState(), 8, 4, 6, 0, 96);
+		}
+
+		if( Options.enablePotash ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.potash.ore.getDefaultState(), 8, 4, 6, 0, 96);
+		}
+
+		if( Options.enableSaltpeter ) {
+			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.saltpeter.ore.getDefaultState(), 8, 4, 6, 0, 96);
+		}
+
 
 		return logic;
 	}
