e293e0b59efe9aa77a08f26dad927ba49565816e
Daniel Hazelton <dshadowwolf@gmail.com>
2017-09-27 13:24:07 -0400

    remove ALT integration as it was not being used at all

--- src/main/java/com/mcmoddev/alt/BaseMineralsALT.java
+++
@@ -1,8 +0,0 @@
-package com.mcmoddev.alt;
-import com.mcmoddev.alt.api.IALTPlugin;
-import com.mcmoddev.alt.api.ALTPlugin;
-
-@ALTPlugin(modid = "baseminerals")
-class BaseMineralsALT implements IALTPlugin {
-	// Intentionally left blank
-}
